94fdc7f98625a76cd11e7b94e86e48244bfe1ed8
kaique <kaiquesilva49@hotmail.com>
2021-08-25 18:27:54 -0300

    layout terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
+import Card from 'react-bootstrap/Card';
+import CardHeader from 'react-bootstrap/CardHeader';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AiOutlineLike } from 'react-icons/ai'
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/posts')
+      .then((response) => {
+        setPosts(response.data)
+      })
+  }, []);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <main>
+        <div className="container">{posts.map((post: any) => {
+          return (
+            <Card key={post.id}>
+              <CardHeader className="card-title"> {post.title}</CardHeader>
+              <div className="container">{post.message}</div>
+              <div className="upvote">
+                {post.upvotes}︁<AiOutlineLike color="black" />
+              </div>
+            </Card>
+          )
+        })}</div>
+      </main>
     </div>
   );
 }
